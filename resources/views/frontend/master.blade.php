793acd619a64fd5ba16cce6a289529d940e869e2
DTP <thanhphuc15052001@gmail.com>
2025-07-29 15:29:50 +0700

    view(layout): create base layout

--- resources/views/frontend/master.blade.php
+++ resources/views/frontend/master.blade.php
@@ -1,1 +1,39 @@
-<h1>hello</h1>
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    @include('frontend.partials.head')
+</head>
+
+<body>
+    <!-- Start Header Area -->
+    @include('frontend.partials.header')
+    <!-- end Header Area -->
+    <main>
+        @yield('content')
+    </main>
+    <!-- Scroll to top start -->
+    <div class="scroll-top not-visible">
+        <i class="fa fa-angle-up"></i>
+    </div>
+    <!-- Scroll to Top End -->
+
+    <!-- footer area start -->
+    @include('frontend.partials.footer')
+    <!-- footer area end -->
+
+    <!-- Quick view modal start -->
+    @include('frontend.partials.quick_view')
+    <!-- Quick view modal end -->
+
+    <!-- offcanvas mini cart start -->
+    @include('frontend.partials.cart_mini')
+    <!-- offcanvas mini cart end -->
+
+    <!-- JS ============================================ -->
+    @include('frontend.partials.js')
+    @include('sweetalert::alert')
+    @stack('scripts')
+</body>
+
+</html>
