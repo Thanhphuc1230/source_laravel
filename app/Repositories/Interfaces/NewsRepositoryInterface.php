803e7fd23982cb41f89768ca0cd2e3582ee2136b
DTP <thanhphuc15052001@gmail.com>
2025-08-29 11:03:41 +0700

    feat: create News Repository Interface

---
+++ app/Repositories/Interfaces/NewsRepositoryInterface.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Repositories\Interfaces;
+
+interface NewsRepositoryInterface extends RepositoryInterface
+{
+    /**
+     * Get filtered news with pagination
+     *
+     * @param array $filters
+     * @param int $perPage
+     * @return \Illuminate\Pagination\LengthAwarePaginator
+     */
+    public function getFilteredNews(array $filters, $perPage = 10);
+
+    /**
+     * Get active categories for news
+     *
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    public function getActiveCategories();
+
+    /**
+     * Generate unique slug for news
+     *
+     * @param string $name
+     * @param string|null $uuid
+     * @return string
+     */
+    public function generateUniqueSlug($name, $uuid = null);
+}
