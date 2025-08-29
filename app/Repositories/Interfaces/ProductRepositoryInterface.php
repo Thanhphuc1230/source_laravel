8f402d36a4fbafb5d35b8c7aecac7efdadd8de4a
DTP <thanhphuc15052001@gmail.com>
2025-08-29 11:03:24 +0700

    feat: Create Product Repository Interface

---
+++ app/Repositories/Interfaces/ProductRepositoryInterface.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Repositories\Interfaces;
+
+interface ProductRepositoryInterface extends RepositoryInterface
+{
+    /**
+     * Get filtered products with pagination
+     *
+     * @param array $filters
+     * @param int $perPage
+     * @return \Illuminate\Pagination\LengthAwarePaginator
+     */
+    public function getFilteredProducts(array $filters, $perPage = 10);
+
+    /**
+     * Get active categories for products
+     *
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    public function getActiveCategories();
+
+    /**
+     * Generate unique slug for product
+     *
+     * @param string $name
+     * @param string|null $uuid
+     * @return string
+     */
+    public function generateUniqueSlug($name, $uuid = null);
+}
