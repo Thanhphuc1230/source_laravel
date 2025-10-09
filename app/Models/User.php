b79e74e36a4a025ee28bfb1a41ab2c76e2bf1e81
DTP <thanhphuc15052001@gmail.com>
2025-10-09 15:48:18 +0700

    feat: integrate HasRoles trait into User model
    - Add HasRoles trait to User model for RBAC functionality
    - Include role and permission relationship methods
    - Enable users to have multiple roles and direct permissions
    - Support permission checking and role management

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Traits\HasRoles;
 use GeneaLabs\LaravelModelCaching\Traits\Cachable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -11,7 +12,7 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, Cachable, HasFactory, Notifiable;
+    use HasApiTokens, Cachable, HasFactory, Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
